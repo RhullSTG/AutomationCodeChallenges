8a4cb06e6e940de8507815d72c6064d46bc642f0
rachael.hull <rachael.hull@stgconsulting.com>
2016-08-10 09:26:15 -0600

    commit

--- AutomationCodeChallenge1/AutomationCodeChallenge1/SkiResortTimes.cs
+++ AutomationCodeChallenge1/AutomationCodeChallenge1/SkiResortTimes.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 
 namespace AutomationCodeChallenge1
@@ -24,11 +25,23 @@ namespace AutomationCodeChallenge1
                 try
                 {
                     driver.FindElement(By.XPath("//*[@id=\"ski-utah-welcome-map\"]/div")).Click();
-                    driver.FindElement(By.XPath("//*[@id=\"ski-utah-welcome-map\"]/div/div[4]/div[2]/div[1]/label[1]")).Click();
+
+                    driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+
+                    driver.FindElement(By.CssSelector("#ski-utah-welcome-map > div > div.map-Container-menu > div.map-Container-compare.is-expanded > label > span.map-Compare-selected.map-Compare-selected--reset")).Click();
+
+                    driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
 
                     IWebElement resortDetails =
-                        driver.FindElement(By.CssSelector(".map-Popover--" + resortName.ToLower()));
-                    IWebElement minutesFromAirport = resortDetails.FindElement(By.XPath("./div[2]/p/span[3]")); 
+                        driver.FindElement(By.XPath("//input[@id=\'map-popover-beaver-mountain\']"));
+                    resortDetails.Click();
+
+                    driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+
+                    var minutesFromAirport = resortDetails.FindElement(By.CssSelector("#ski-utah-welcome-map > div > div.map-Container-content > div.map-Areas > div.map-Area.map-Area--logan > div.map-Popover.map-Popover--beaver-mountain > div.map-Popover-content > p")).Text;
+
+                    Console.WriteLine(minutesFromAirport);
+                
                 }
                 catch (ElementNotVisibleException ex)
                 { 
